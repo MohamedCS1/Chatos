a8dd35bfec08717f87443cc1c53ad4a11cca1716
MohamedCS1 <mezennermohamed56@gmail.com>
2022-03-27 13:03:03 +0100

    add image selection from internal storage

--- app/src/main/java/com/example/messenger/ProfileActivity.kt
+++ app/src/main/java/com/example/messenger/ProfileActivity.kt
@@ -1,11 +1,44 @@
 package com.example.messenger
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.messenger.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityProfileBinding
+    lateinit var arl: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.profileImageBig.setOnClickListener {
+            val intentImage = Intent().apply {
+                type = "image/*"
+                action = Intent.ACTION_GET_CONTENT
+                putExtra(Intent.EXTRA_MIME_TYPES , arrayOf("image/jpeg" ,"image/png"))
+            }
+
+            arl.launch(intentImage)
+        }
+
+        arl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult() ,object :ActivityResultCallback<ActivityResult>{
+                override fun onActivityResult(result: ActivityResult?) {
+                    if (result?.resultCode == Activity.RESULT_OK && result.data != null)
+                    {
+                        binding.profileImageBig.setImageURI(result.data!!.data)
+                    }
+                }
+            })
+
     }
 }
