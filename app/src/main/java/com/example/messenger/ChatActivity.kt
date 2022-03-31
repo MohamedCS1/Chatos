ac599507f8e17b8b4b0d5dbcd415d20d0bb26013
MohamedCS1 <mezennermohamed56@gmail.com>
2022-03-31 23:33:48 +0100

    pass data to chat activity

--- app/src/main/java/com/example/messenger/ChatActivity.kt
+++ app/src/main/java/com/example/messenger/ChatActivity.kt
@@ -3,19 +3,26 @@ package com.example.messenger
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.example.messenger.databinding.ActivityChatBinding
 import com.example.pojo.Person
 
 class ChatActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityChatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         val bundle = intent.extras
 
         val person = bundle?.get("person") as Person
 
-        Toast.makeText(this ,person.toString() ,Toast.LENGTH_SHORT).show()
+        binding.tvUsername.text = person.name
+        Glide.with(this).load(person.imagePath).placeholder(R.drawable.ic_photo_placeholder).into(binding.imageviewPhotoProfile)
 
     }
 }
