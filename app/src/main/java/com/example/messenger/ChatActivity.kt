bbf6d59067198f0e27973022d9dc3e4145eafc47
MohamedCS1 <mezennermohamed56@gmail.com>
2022-04-01 14:38:03 +0100

    fix probelm in bottom tolbar

--- app/src/main/java/com/example/messenger/ChatActivity.kt
+++ app/src/main/java/com/example/messenger/ChatActivity.kt
@@ -1,31 +1,105 @@
 package com.example.messenger
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.DisplayMetrics
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.marginRight
 import com.bumptech.glide.Glide
 import com.example.messenger.databinding.ActivityChatBinding
 import com.example.pojo.Person
 
+
 class ChatActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityChatBinding
+    lateinit var context: Context
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        context = this
 
         val bundle = intent.extras
 
         val person = bundle?.get("person") as Person
 
+        bottomToolbarSendMessageAnimation()
         Toast.makeText(this ,person.uid ,Toast.LENGTH_SHORT).show()
 
 
         binding.tvUsername.text = person.name
         Glide.with(this).load(person.imagePath).placeholder(R.drawable.ic_photo_placeholder).into(binding.imageviewPhotoProfile)
 
+        binding.toolBarBuChat.setTextColor(Color.parseColor("#51BA65"))
+        binding.toolBarBuChat.setBackgroundResource(R.drawable.shape_circle)
+        binding.toolBarBuFiles.setTextColor(Color.parseColor("#FFFFFFFF"))
+        binding.toolBarBuFiles.setBackgroundResource(R.color.my_green)
+
+        binding.toolBarBuChat.setOnClickListener {
+            binding.toolBarBuChat.setTextColor(Color.parseColor("#51BA65"))
+            binding.toolBarBuChat.setBackgroundResource(R.drawable.shape_circle)
+            binding.toolBarBuFiles.setTextColor(Color.parseColor("#FFFFFFFF"))
+            binding.toolBarBuFiles.setBackgroundResource(R.color.my_green)
+        }
+
+        binding.toolBarBuFiles.setOnClickListener {
+            binding.toolBarBuFiles.setTextColor(Color.parseColor("#51BA65"))
+            binding.toolBarBuFiles.setBackgroundResource(R.drawable.shape_circle)
+            binding.toolBarBuChat.setTextColor(Color.parseColor("#FFFFFFFF"))
+            binding.toolBarBuChat.setBackgroundResource(R.color.my_green)
+        }
+
     }
+
+    fun bottomToolbarSendMessageAnimation()
+    {
+
+        binding.icSendMessage.visibility = View.INVISIBLE
+
+        val margin = resources.getDimension(R.dimen.text_margin).toInt()
+        val layoutParams = CoordinatorLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        binding.edittextSendMessage.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s!!.isNotBlank() && s.isNotEmpty())
+                {
+                    layoutParams.setMargins(0,0,margin ,0)
+                    binding.icSendMessage.visibility = View.VISIBLE
+                    binding.edittextSendMessage.layoutParams = layoutParams
+                }
+                else
+                {
+                    layoutParams.setMargins(0, 0, 0, 0)
+                    binding.icSendMessage.visibility = View.INVISIBLE
+                    binding.edittextSendMessage.layoutParams = layoutParams
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
 }
