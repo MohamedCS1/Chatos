aeba54cd0ef090d09d81f2dff3c1e5a1a63deebb
MohamedCS1 <mezennermohamed56@gmail.com>
2022-04-06 05:40:05 +0100

    starting create public profile activity

---
+++ app/src/main/java/com/example/messenger/PublicProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PublicProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_public_profile)
+    }
+}
